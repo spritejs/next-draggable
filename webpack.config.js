1ae607d43ec21d571a4ffc19bf9deae19f2fafd1
yaotaiyang <yaotaiyang99@gmail.com>
2019-11-26 17:40:03 +0800

    modify dev-env

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,25 @@
-const path = require('path');
-const fs = require('fs');
-const webpack = require('webpack');
-const HTMLPlugin = require('html-webpack-plugin')
+const path = require('path')
 
-let babelConf;
-if (fs.existsSync('./.babelrc')) {
-  // use babel
-  babelConf = JSON.parse(fs.readFileSync('.babelrc'));
-}
-
-module.exports = function (env = {}) {
-  const externals = {};
-  let filename = 'sprite-draggable.standalone.js';
-  if (!env.standalone) {
-    externals[ 'sprite-core' ] = 'spritejs';
-    filename = 'sprite-draggable.js';
+module.exports = {
+  mode: 'development',
+  entry: {
+    demo: './examples/demo.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '/'),
+    filename: '[name].js',
+    library: 'spriteDraggable',
+    libraryTarget: 'umd'
+  },
+  devtool: 'source-map',
+  externals: {
+    spritejs: 'spritejs'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'examples'),
+    open: true
+    //hot: true
+    //compress: true
+    //port: 9000
   }
-
-  return {
-    mode: 'development',
-    entry: './example/index',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: babelConf,
-          },
-        },
-      ],
-
-      /* Advanced module configuration (click to show) */
-    },
-
-    externals,
-    // Don't follow/bundle these modules, but request them at runtime from the environment
-
-    stats: 'errors-only',
-    // lets you precisely control what bundle information gets displayed
-
-    devServer: {
-      contentBase: path.join(__dirname, 'example'),
-      compress: true,
-      port: 9092,
-      hot: true
-      // ...
-    },
-
-    plugins: [
-      // ...
-      new HTMLPlugin({
-        filename: "index.html",
-        template: './example/test.html',
-        inject: true
-      }),
-      new webpack.NamedModulesPlugin(),
-      new webpack.HotModuleReplacementPlugin()
-    ]
-  };
-};
+}
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,52 +1,22 @@
-const path = require('path');
-const fs = require('fs');
-const webpack = require('webpack');
-const HTMLPlugin = require('html-webpack-plugin')
+const path = require('path')
 
-let babelConf;
-if (fs.existsSync('./.babelrc')) {
-  // use babel
-  babelConf = JSON.parse(fs.readFileSync('.babelrc'));
-}
-
-module.exports = function (env = {}) {
-  const externals = {};
-  let filename = 'sprite-draggable.standalone.js';
-  if (!env.standalone) {
-    externals[ 'sprite-core' ] = 'spritejs';
-    filename = 'sprite-draggable.js';
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'lib'),
+    library: 'spriteDraggable',
+    libraryTarget: 'umd'
+  },
+  devtool: 'source-map',
+  externals: {
+    spritejs: 'spritejs'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'examples'),
+    hot: true
   }
-
-  return {
-    mode: 'production',
-    entry: {
-      index: path.resolve(__dirname, './src/index')
-    },
-    output: {
-      path: path.resolve(__dirname, './lib'),
-      filename: '[name].js',
-      library: 'spriteDraggable',
-      libraryTarget: 'umd'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: babelConf,
-          },
-        },
-      ],
-
-      /* Advanced module configuration (click to show) */
-    }
-    // lets you precisely control what bundle information gets displayed
-
-    // list of additional plugins
-
-
-    /* Advanced configuration (click to show) */
-  };
-};
+}
