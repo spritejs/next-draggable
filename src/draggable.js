e9b4b64aa3686f13a8d24d78f29f596a88538c8c
yaotaiyang <yaotaiyang99@gmail.com>
2020-06-15 14:49:12 +0800

    添加拖动容差

--- src/draggable.js
+++ src/draggable.js
@@ -18,20 +18,14 @@ export function draggable(sprite, option) {
     //销毁draggable
     if (!sprite[_isDraggable]) return sprite
     delete sprite[_isDraggable]
-    return sprite
-      .removeEventListener('mousedown', sprite[_mouseDown], false)
-      .removeEventListener('mousemove', sprite[_mouseMove], false)
-      .removeEventListener('mouseup', sprite[_mouseUp], false)
+    return sprite.removeEventListener('mousedown', sprite[_mouseDown], false).removeEventListener('mousemove', sprite[_mouseMove], false).removeEventListener('mouseup', sprite[_mouseUp], false)
   } else {
     if (!sprite[_isDraggable]) {
       sprite[_isDraggable] = true
       sprite[_mouseDown] = mouseDown
       sprite[_mouseMove] = mouseMove
       sprite[_mouseUp] = mouseUp
-      return sprite
-        .addEventListener('mousedown', sprite[_mouseDown], false)
-        .addEventListener('mousemove', sprite[_mouseMove], false)
-        .addEventListener('mouseup', sprite[_mouseUp], false)
+      return sprite.addEventListener('mousedown', sprite[_mouseDown], false).addEventListener('mousemove', sprite[_mouseMove], false).addEventListener('mouseup', sprite[_mouseUp], false)
     }
   }
   function mouseDown(evt) {
@@ -44,6 +38,7 @@ export function draggable(sprite, option) {
     const [offsetX, offsetY] = sprite.getOffsetPosition(evt.x, evt.y)
     $drag.x0_ = offsetX
     $drag.y0_ = offsetY
+    $drag._dragStartPoint = [evt.x, evt.y]
     $drag.dispatchEvent('dragstart', transEvent(evt), true, true)
     $drag.setMouseCapture()
   }
@@ -51,6 +46,11 @@ export function draggable(sprite, option) {
   function mouseMove(evt) {
     if ($drag && $drag === sprite && $drag.x0_ != null) {
       const [offsetX, offsetY] = sprite.getOffsetPosition(evt.x, evt.y)
+      let [dragStartX, dragStartY] = sprite._dragStartPoint
+      //添加容差处理，如果拖动小于2，默认不处理
+      if (Math.sqrt(Math.pow(evt.x - dragStartX, 2) + Math.pow(evt.y - dragStartY, 2)) < 2) {
+        return
+      }
       let dx = offsetX - sprite.x0_
       let dy = offsetY - sprite.y0_
       const [cx, cy] = sprite.attr('pos')
